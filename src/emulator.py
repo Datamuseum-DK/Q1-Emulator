6bfddd6265b0ffb817ac5a21e50efa0ddd3ef97a
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-08-29 00:00:00 +0200

    rtc io

--- src/emulator.py
+++ src/emulator.py
@@ -9,6 +9,7 @@ import ros as r
 import z80io
 import programs as prg
 import disks.debugdisk.image as ddim
+from timeit import default_timer as timer
 
 '''
     Q1 Emulator
@@ -45,7 +46,12 @@ def emulator(args):
     if args.hexdump:
         cpu.mem.hexdump(0x2000, 0xFFFF - 0x2000) # dump RAM part of memory
 
+    tstart = timer()
     while True:
+        tend = timer()
+        if (tend - tstart) > 1.0:
+            tstart = timer()
+            io.timeout = True
         pc = cpu.m.pc
         icount += 1
         if icount >= args.stopafter or pc > 65530:
