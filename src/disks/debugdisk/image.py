0a26c884f9f59a1f24e8a5b3f75f4381637e77c0
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-11-12 14:48:19 +0100

    temporary commmit, rename filesystem objects

--- src/disks/debugdisk/image.py
+++ src/disks/debugdisk/image.py
@@ -3,34 +3,34 @@ import sys
 sys.path.insert(0, '../..')
 
 # tracks 5 - 9 still not ready
-from disks.debugdisk import t0, t1, t2, t3, t4 #, t5, t6, t7, t8, t9
+from disks.debugdisk import t0, t1, t2, t3, t4, t5, t6 #, t7, t8, t9
 import filesys
 
 
-ddfs = filesys.FileSys()
-ddfs.loadtracks([t0.data, t1.data, t2.data, t3.data, t4.data #,
-                 # t5.data, t6.data, t7.data, t8.data, t9.data
+fs = filesys.FileSys()
+fs.loadtracks([t0.data, t1.data, t2.data, t3.data, t4.data,
+                 t5.data, t6.data#, t7.data, t8.data, t9.data
                  ])
 
 
 if __name__ == '__main__':
     track = filesys.Track()
-    track.info(0, ddfs.data[0], 130, 40)
-    track.info(1, ddfs.data[1], 30, 255)
-    track.info(2, ddfs.data[2], 30, 255)
-    track.info(3, ddfs.data[3], 30, 255)
-    track.info(4, ddfs.data[4], 30, 255)
-    # track.info(5, ddfs.data[5], 30, 255)
-    # track.info(6, ddfs.data[6], 30, 255)
-    # track.info(7, ddfs.data[7], 30, 255)
-    # track.info(8, ddfs.data[8], 30, 255)
-    # track.info(9, ddfs.data[9], 30, 255)
+    track.info(0, fs.data[0], 130, 40)
+    track.info(1, fs.data[1], 30, 255)
+    track.info(2, fs.data[2], 30, 255)
+    track.info(3, fs.data[3], 30, 255)
+    track.info(4, fs.data[4], 30, 255)
+    track.info(5, fs.data[5], 30, 255)
+    # track.info(6, fs.data[6], 30, 255)
+    # track.info(7, fs.data[7], 30, 255)
+    # track.info(8, fs.data[8], 30, 255)
+    # track.info(9, fs.data[9], 30, 255)
 
-    # print(ddfs.data[0][:48])
+    # print(fs.data[0][:48])
     # print()
-    # print(ddfs.data[0][48:96])
+    # print(fs.data[0][48:96])
     # print()
-    # print(ddfs.data[1][:263])
-    # print(ddfs.data[1][263:293])
-    #print(ddfs.marks[1])
-    #print(ddfs.data)
+    # print(fs.data[1][:263])
+    # print(fs.data[1][263:293])
+    #print(fs.marks[1])
+    #print(fs.data)
