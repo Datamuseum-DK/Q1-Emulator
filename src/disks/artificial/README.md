fe98cfeeeeda2ba4b7d9d1ebc86079be2b6ba5c5
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-11-18 14:29:40 +0100

    addind tracks for artificial disk image

---
+++ src/disks/artificial/README.md
@@ -0,0 +1,8 @@
+
+
+Artificially created disk.
+
+
+Track 0 - INDEX
+Track 1 - TT1R10 - text file, one track, 10 records, 79 recsize (PL/1)
+Track 2 - TT2R10 - text file, one track, 10 records, 79 recsize (PL/1)
---
+++ src/disks/artificial/image.py
@@ -0,0 +1,18 @@
+
+import sys
+sys.path.insert(0, '../..')
+
+from disks.artificial import t0, t1, t2
+import filesys
+
+tracks = [t0.data, t1.data, t2.data]
+
+fs = filesys.FileSys()
+fs.loadtracks(tracks) #
+
+
+if __name__ == '__main__':
+    track = filesys.Track()
+    track.info( 0, fs.data[0],   16,  40)
+    track.info( 1, fs.data[1],   10,  79)
+    track.info( 2, fs.data[2],   10,  79)
